d8d9961a7fe74f790b20332c28dda98853f9f547
Ha <99289909+hawintha@users.noreply.github.com>
2022-07-06 20:13:29 -0700

    Allow players to take turns marking the board

--- games/tic-tac-toe.js
+++ games/tic-tac-toe.js
@@ -2,11 +2,43 @@ const gameBoard = (function () {
     let marks = ["", "", "", "", "", "", "", "", ""];
 
     const tilesGrid = document.querySelector('#tilesGrid');
-    marks.forEach(() => {
+    for (let i = 1; i < 10; i++) {
         const tile = document.createElement('div');
         tile.classList.add("tile");
+        tile.setAttribute('data-index', i);
         tilesGrid.appendChild(tile);
-    })
+    }
 
-    return { marks };
+    const tiles = document.querySelectorAll('.tile');
+    tiles.forEach((tile) => {
+        tile.addEventListener('click', (e) => {
+            game.play(e.target, e.target.dataset.index, game.currentPlayer);
+        });
+    });
+
+    function markTile(tile, index, marker) {
+        marks[index] = marker;
+        tile.innerText = marker;
+    }
+
+    return { markTile };
+})();
+
+const createPlayer = (marker) => {
+    this.marker = marker;
+    return { marker };
+};
+
+const game = (function () {
+    const p1 = createPlayer("X");
+    const p2 = createPlayer("O");
+    let currentPlayer = p1;
+
+    const alertedPlayer = document.querySelector('#alertedPlayer');
+    function play(tile, index, player) {
+        gameBoard.markTile(tile, index, player.marker);
+        player === p1 ? this.currentPlayer = p2 : this.currentPlayer = p1;
+        alertedPlayer.innerText = this.currentPlayer.marker;
+    }
+    return { currentPlayer, play };
 })();
