1bb51ed4c8fdc09d65b2555d49d0e6511a7b66b9
Ha <99289909+hawintha@users.noreply.github.com>
2022-08-02 11:56:05 -0700

    Alert when there is a tie and reset game after a tie or win

--- games/tic-tac-toe.js
+++ games/tic-tac-toe.js
@@ -1,22 +1,27 @@
 const gameBoard = (function () {
     let marks = [];
-    const tilesGrid = document.querySelector('#tilesGrid');
-    for (let i = 0; i < 9; i++) {
-        marks.push("");
-        const tile = document.createElement('div');
-        tile.classList.add("tile");
-        tile.setAttribute('data-index', i);
-        tilesGrid.appendChild(tile);
-    }
 
-    const tiles = document.querySelectorAll('.tile');
-    tiles.forEach((tile) => {
-        tile.addEventListener('click', (e) => {
-            if (!e.target.innerText) {
-                game.play(e.target, e.target.dataset.index, game.currentPlayer, game.currentPlayer.marker);
-            }
+    function newBoard() {
+        const tilesGrid = document.querySelector('#tilesGrid');
+        marks = [];
+        tilesGrid.replaceChildren();
+        for (let i = 0; i < 9; i++) {
+            marks.push("");
+            const tile = document.createElement('div');
+            tile.classList.add("tile");
+            tile.setAttribute('data-index', i);
+            tilesGrid.appendChild(tile);
+        }
+        const tiles = document.querySelectorAll('.tile');
+        tiles.forEach((tile) => {
+            tile.addEventListener('click', (e) => {
+                if (!e.target.innerText) {
+                    game.play(e.target, e.target.dataset.index, game.currentPlayer, game.currentPlayer.marker);
+                }
+            });
         });
-    });
+    }
+    newBoard();
 
     function markTile(tile, index, marker) {
         marks[index] = marker;
@@ -33,10 +38,20 @@ const gameBoard = (function () {
             (marks[2] === marker && marks[5] === marker && marks[8] == marker) ||
             (marks[0] === marker && marks[4] === marker && marks[8] == marker) ||
             (marks[2] === marker && marks[4] === marker && marks[6] == marker)
-        ) { alert(`${marker} has won!`) }
+        ) {
+            alert(`${marker} has won!`);
+            newBoard();
+        }
     }
-    
-    return { markTile, checkWin };
+
+    function checkTie() {
+        if (!marks.includes("", 0)) {
+            alert("It's a tie!");
+            newBoard();
+        }
+    }
+
+    return { newBoard, markTile, checkWin, checkTie };
 })();
 
 const createPlayer = (marker) => {
@@ -53,6 +68,7 @@ const game = (function () {
     function play(tile, index, player, marker) {
         gameBoard.markTile(tile, index, marker);
         gameBoard.checkWin(marker);
+        gameBoard.checkTie();
         player === p1 ? this.currentPlayer = p2 : this.currentPlayer = p1;
         alertedPlayer.innerText = this.currentPlayer.marker;
     }
