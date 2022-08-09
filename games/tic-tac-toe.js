943a089b9c041c5019989fea9c7890c5e6688364
Ha <99289909+hawintha@users.noreply.github.com>
2022-08-09 15:50:02 -0700

    Allow players to customize their names and reset scoreboard

--- games/tic-tac-toe.js
+++ games/tic-tac-toe.js
@@ -1,6 +1,5 @@
 const gameBoard = (function () {
     let marks = [];
-
     function newBoard() {
         const tilesGrid = document.querySelector('#tilesGrid');
         marks = [];
@@ -16,43 +15,42 @@ const gameBoard = (function () {
         tiles.forEach((tile) => {
             tile.addEventListener('click', (e) => {
                 if (!e.target.innerText) {
-                    game.play(e.target, e.target.dataset.index, game.currentPlayer, game.currentPlayer.marker);
+                    markTile(e.target, e.target.dataset.index, game.currentPlayer.mark);
+                    checkWin(game.currentPlayer.mark);
+                    checkTie();
+                    game.changePlayer(game.currentPlayer);
                 }
             });
         });
     }
-    newBoard();
-
-    function markTile(tile, index, marker) {
-        marks[index] = marker;
-        tile.innerText = marker;
+    function markTile(tile, index, mark) {
+        marks[index] = mark;
+        tile.innerText = mark;
     }
 
     const p1Score = document.querySelector('#p1Score');
     const p2Score = document.querySelector('#p2Score');
     const ties = document.querySelector('#ties');
-
-    function checkWin(marker) {
+    function checkWin(mark) {
         if (
-            (marks[0] === marker && marks[1] === marker && marks[2] == marker) ||
-            (marks[3] === marker && marks[4] === marker && marks[5] == marker) ||
-            (marks[6] === marker && marks[7] === marker && marks[8] == marker) ||
-            (marks[0] === marker && marks[3] === marker && marks[6] == marker) ||
-            (marks[1] === marker && marks[4] === marker && marks[7] == marker) ||
-            (marks[2] === marker && marks[5] === marker && marks[8] == marker) ||
-            (marks[0] === marker && marks[4] === marker && marks[8] == marker) ||
-            (marks[2] === marker && marks[4] === marker && marks[6] == marker)
+            (marks[0] === mark && marks[1] === mark && marks[2] == mark) ||
+            (marks[3] === mark && marks[4] === mark && marks[5] == mark) ||
+            (marks[6] === mark && marks[7] === mark && marks[8] == mark) ||
+            (marks[0] === mark && marks[3] === mark && marks[6] == mark) ||
+            (marks[1] === mark && marks[4] === mark && marks[7] == mark) ||
+            (marks[2] === mark && marks[5] === mark && marks[8] == mark) ||
+            (marks[0] === mark && marks[4] === mark && marks[8] == mark) ||
+            (marks[2] === mark && marks[4] === mark && marks[6] == mark)
         ) {
-            alert(`${marker} has won!`);
-            if (marker === "X") {
+            alert(`${game.currentPlayer.nickname} has won!`);
+            if (mark === "X") {
                 p1Score.innerText++;
-            } else if (marker === "O") {
+            } else if (mark === "O") {
                 p2Score.innerText++;
             }
             newBoard();
         }
     }
-
     function checkTie() {
         if (!marks.includes("", 0)) {
             alert("It's a tie!");
@@ -60,28 +58,45 @@ const gameBoard = (function () {
             newBoard();
         }
     }
+    const resetBtn = document.querySelector('#resetBtn');
+    resetBtn.addEventListener('click', () => {
+        p1Score.innerText = 0;
+        p2Score.innerText = 0;
+    })
 
-    return { newBoard, markTile, checkWin, checkTie };
+    return { newBoard };
 })();
 
-const createPlayer = (marker) => {
-    this.marker = marker;
-    return { marker };
-};
-
 const game = (function () {
+    function createPlayer(mark) {
+        this.mark = mark;
+        this.nickname = `Player ${mark}`
+        return { mark, nickname };
+    };
     const p1 = createPlayer("X");
     const p2 = createPlayer("O");
     let currentPlayer = p1;
-    const alertedPlayer = document.querySelector('#alertedPlayer');
-
-    function play(tile, index, player, marker) {
-        gameBoard.markTile(tile, index, marker);
-        gameBoard.checkWin(marker);
-        gameBoard.checkTie();
-        player === p1 ? this.currentPlayer = p2 : this.currentPlayer = p1;
-        alertedPlayer.innerText = this.currentPlayer.marker;
+    function customizeNames(p1Input, p2Input) {
+        const playerNotified = document.querySelector('#playerNotified');
+        if (p1Input.value) p1.nickname = p1Input.value;
+        if (p2Input.value) p2.nickname = p2Input.value;
+        playerNotified.innerText = currentPlayer.nickname;
     }
 
-    return { currentPlayer, play };
+    const playBtn = document.querySelector('#playBtn');
+    const settings = document.querySelector('#settings');
+    const main = document.querySelector('main');
+    playBtn.addEventListener('click', () => {
+        gameBoard.newBoard();
+        main.style.display = "inline-block";
+        settings.style.display = "none";
+        customizeNames(document.querySelector('#p1Name'), document.querySelector('#p2Name'));
+    });
+
+    function changePlayer(player) {
+        player === p1 ? this.currentPlayer = p2 : this.currentPlayer = p1;
+        playerNotified.innerText = this.currentPlayer.nickname;
+    }
+
+    return { currentPlayer, changePlayer };
 })();
